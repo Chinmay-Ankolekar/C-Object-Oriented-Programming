aa39903fd7d6d80525976439bf2febe4b8de2658
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-02-22 15:33:18 +0530

    Update 14. Publication.cpp

--- 14. Publication.cpp
+++ 14. Publication.cpp
@@ -1,12 +1,3 @@
-/*Define a class Publication which has a title. Derive two classes from it – a 
-class Book which has an accession number and a class Magazine which 
-has volume number.With these two as bases,derive the class Journal. 
-Define a function print() in each of these classes.Ensure that the derived 
-class function always invokes the base(s) class function. In main() create 
-a Journal called IEEEOOP with an accession number 681.3 and a volume 
-number 1.Invoke the print() function for this object
-*/
-
 #include<iostream>
 using namespace std;
 
@@ -53,10 +44,3 @@ int main(){
     IEEEOOP.print();
     return 0;
 }
-
-/*
-OUTPUT:-
-TITLE : IEEEOOP
-ACCESSION NUMBER : 681.3
-VOLUME : 1
-*/
