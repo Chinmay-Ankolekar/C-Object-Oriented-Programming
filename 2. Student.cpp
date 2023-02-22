4c2d0e9f29c35e91c2aefba0ec6e194e2f57011f
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-02-22 15:34:23 +0530

    Update 2. Student.cpp

--- 2. Student.cpp
+++ 2. Student.cpp
@@ -1,9 +1,3 @@
-/*2)Define a STUDENT class with USN, Name, and Marks in 3 tests of a 
-subject.
-Declare an array of 10 STUDENT objects. Using appropriate functions, 
-find the average of two better marks for each student. Print the USN, 
-Name and the average marks of all the students.*/
-
 #include<iostream>
 using namespace std;
 #define SIZE 10
@@ -58,32 +52,3 @@ int main()
     cout<<"\n------------";
   }
 }
-
-/*
-  OUTPUT:-
-
-Students Report
-Enter the number of students: 2
-
-Enter student USN: 1
-Enter student name: Chinmay
-Enter student's 3 marks: 60 70 80
-
-Enter student USN: 2
-Enter student name: ABC
-Enter student's 3 marks: 80 90 50
-
------------------
-Students Details::
------------------
-Student:1
-Student USN: 1
-Student name: Chinmay
-Student average: 75
-------------
-Student:2
-Student USN: 2
-Student name: ABC
-Student average: 85
-------------
-*/
